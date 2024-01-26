067d164c6dd047d078d7cce11ef6c6de208ac901
Punkachu <ovila.lugard@gmail.com>
2024-01-26 17:50:28 +0100

    ✨ complete widget with sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'space_floating_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,115 +15,131 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Space Floating Widget',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          backgroundColor: Color(0xFF00258D),
+          // backgroundColor: Colors.black45,
+          appBar: AppBar(
+            title: const Text(
+              "Vibing In the Floating Space",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ),
+          body: _generateGrid()),
+    );
+  }
+
+  Widget _generateGrid() {
+    final List<IconData> icons = [
+      Icons.add,
+      Icons.account_circle_outlined,
+      Icons.access_time_sharp,
+      Icons.add_circle_outline,
+      Icons.radar_rounded,
+      Icons.radio,
+      Icons.connected_tv_sharp,
+      Icons.accessible_rounded,
+      Icons.person,
+      Icons.face,
+    ];
+    return GridView.builder(
+      padding: const EdgeInsets.all(20),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
+      itemCount: 10, // Number of items in the grid
+      itemBuilder: (BuildContext context, int index) {
+        return GridTile(
+          child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: SpaceFloatingWidget(
+                      //padding: const EdgeInsets.only(left: 40, top: 30),
+                      widget: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CustomPaint(
+                        painter: CustomRoundShape(
+                          80,
+                          gradientColors: const [
+                            Color(0xff4d4dff),
+                            Color(0xff400080),
+                          ],
+                        ),
+                      ),
+                      Icon(icons.elementAt(index),
+                          size: 50, color: Colors.white),
+                    ],
+                  )),
+                ),
+                //const SizedBox(height: 8.0),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 22,
+                      sigmaY: 22,
+                    ),
+
+                    /// Filter effect field
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Item $index',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CustomRoundShape extends CustomPainter {
+  final double radius;
+  final List<Color> gradientColors;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  CustomRoundShape(this.radius,
+      {this.gradientColors = const [
+        Color(0xff4d4dff),
+        Color(0xff400080),
+      ]});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..shader = LinearGradient(
+        colors: gradientColors,
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        tileMode: TileMode.mirror,
+      ).createShader(Rect.fromCircle(
+        center: const Offset(0, 0),
+        radius: radius,
+      ));
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    canvas.drawCircle(Offset.zero, radius, paint);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
---
+++ lib/space_floating_widget.dart
@@ -0,0 +1,172 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SpaceFloatingWidget extends StatefulWidget {
+  const SpaceFloatingWidget({
+    required this.widget,
+    this.leftOffsetAnimation = const Offset(.23, .27),
+    this.topOffsetAnimation = const Offset(.21, .28),
+    this.animationTopDuration = const Duration(milliseconds: 2500),
+    this.animationLeftDuration = const Duration(milliseconds: 2500),
+    this.curve = Curves.easeInOut,
+    this.padding,
+    super.key,
+  });
+
+  /// Widget You want to make floating in space !
+  final Widget widget;
+
+  /// Animations duration
+  final Duration animationTopDuration;
+  final Duration animationLeftDuration;
+
+  /// offset for Tween animations
+  final Offset? leftOffsetAnimation;
+  final Offset? topOffsetAnimation;
+
+  /// Curve for Tween animations
+  final Curve curve;
+
+  /// Padding for the inner widget
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  State<SpaceFloatingWidget> createState() => _SpaceFloatingWidgetState();
+}
+
+class _SpaceFloatingWidgetState extends State<SpaceFloatingWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController controllerLeft;
+  late final AnimationController controllerTop;
+
+  late final Animation<double> animationLeft;
+  late final Animation<double> animationTop;
+
+  late final Random random = Random();
+
+  void _initialize() {
+    final Duration _animationTopDuration = widget.animationTopDuration ??
+        Duration(
+            milliseconds: widget.animationTopDuration.inMilliseconds +
+                random.nextInt(1000));
+    final Duration _animationLeftDuration = widget.animationLeftDuration ??
+        Duration(
+            milliseconds: widget.animationTopDuration.inMilliseconds +
+                random.nextInt(1000));
+
+    final double randomLeft = random.nextInt(100) / 100;
+    final double randomTop = random.nextInt(100) / 100;
+
+    final Offset leftOffsetAnimation = widget.leftOffsetAnimation ??
+        Offset(.23 + randomLeft, .27 + randomLeft);
+
+    final Offset topOffsetAnimation =
+        widget.leftOffsetAnimation ?? Offset(.23 + randomTop, .27 + randomTop);
+
+    controllerLeft =
+        AnimationController(vsync: this, duration: _animationTopDuration);
+
+    animationLeft = Tween<double>(
+            begin: leftOffsetAnimation.dx, end: leftOffsetAnimation.dy)
+        .animate(
+      CurvedAnimation(
+        parent: controllerLeft,
+        curve: widget.curve,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controllerLeft.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controllerLeft.forward();
+        }
+      });
+
+    controllerTop =
+        AnimationController(vsync: this, duration: _animationTopDuration);
+
+    animationTop =
+        Tween<double>(begin: topOffsetAnimation.dx, end: topOffsetAnimation.dy)
+            .animate(CurvedAnimation(
+      parent: controllerTop,
+      curve: widget.curve,
+    ))
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              controllerTop.reverse();
+            } else if (status == AnimationStatus.dismissed) {
+              controllerTop.forward();
+            }
+          });
+
+    Timer(_animationLeftDuration, () {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (mounted) {
+          controllerLeft.forward();
+        }
+      });
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        controllerTop.forward();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  @override
+  void dispose() {
+    controllerLeft.dispose();
+    controllerTop.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      key: const Key("AnimatedWidget"),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          double maxWidth = constraints.maxWidth;
+          double maxHeight = constraints.maxHeight;
+
+          return Container(
+            height: maxHeight,
+            width: maxWidth,
+            padding: widget.padding ??
+                EdgeInsets.only(
+                  top: (maxHeight / 2) * 0.15,
+                  left: (maxWidth / 2) * 0.2,
+                ),
+            alignment: Alignment.center,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  top: ((maxHeight / 2)) * animationTop.value,
+                  left: (maxWidth / 2) * animationLeft.value,
+                  child: widget.widget,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
